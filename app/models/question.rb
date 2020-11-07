4d8dc9cead854f2059afd929a293a1bf5e129ccd
Haruki Hirai <iamprivateengineer@gmail.com>
2020-11-07 13:57:55 +0900

    アソシエーション、バリデーションとカラムの追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,10 @@
 class Question < ApplicationRecord
+  
+  belongs_to :user
+
+  with_options presence: true do
+    validates :category_id, numericality: { other_than: 0 }
+    validates :title
+    validates :text
+  end
 end
--- db/migrate/20201107030232_create_questions.rb
+++ db/migrate/20201107030232_create_questions.rb
@@ -1,7 +1,10 @@
 class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
-
+      t.integer :category_id, null: false
+      t.string  :title, null: false
+      t.text    :text, null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
