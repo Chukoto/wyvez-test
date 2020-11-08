5d17d88d9ee6e66be99c89b2e3b0c0a708d224bd
Haruki Hirai <iamprivateengineer@gmail.com>
2020-11-08 15:00:53 +0900

    Bookmark用ルーティング既述

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'questions#index'
 
+  post "bookmarks/:question_id/create" => "bookmarks#create"
+  post "bookmarks/:question_id/destroy" => "bookmarks#destroy"
+
   resources :questions do
     resources :comments, only: [:create, :destroy]
+    resources :bookmarks, only: [:create, :destroy]
   end
 end
