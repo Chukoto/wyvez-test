2bd215b756020068438e93014ea074c51d915161
Haruki Hirai <iamprivateengineer@gmail.com>
2020-11-08 15:01:25 +0900

    カラムに_id付与

--- db/migrate/20201107102359_create_bookmarks.rb
+++ db/migrate/20201107102359_create_bookmarks.rb
@@ -1,8 +1,8 @@
 class CreateBookmarks < ActiveRecord::Migration[6.0]
   def change
     create_table :bookmarks do |t|
-      t.integer :user, null: false
-      t.integer :question, null: false
+      t.integer :user_id, null: false
+      t.integer :question_id, null: false
       t.timestamps
     end
   end
