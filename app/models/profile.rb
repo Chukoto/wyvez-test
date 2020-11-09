4d543781a4adaf623496174e22190adbfe585f06
Haruki Hirai <iamprivateengineer@gmail.com>
2020-11-09 23:30:02 +0900

    プルダウン用のバリデーション記述

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,14 +1,17 @@
 class Profile < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :experience
+  belongs_to_active_hash :trans_exp
+  belongs_to_active_hash :papa_exp
 
   belongs_to :user
 
   has_one_attached :image
 
+  validates :text, presence: false
+
   with_options presence: true do
-    validates :text
-    validates :category_id, numericality: { other_than: 0 }
+    validates :trans_exp_id, numericality: { other_than: 0 }
+    validates :papa_exp_id, numericality: { other_than: 0 }
   end
 
   def was_attached?
