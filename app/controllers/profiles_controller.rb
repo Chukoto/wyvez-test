122bb94fd08e9a35bec4122c4297677b44c38081
Haruki Hirai <iamprivateengineer@gmail.com>
2020-11-09 23:06:17 +0900

    コントローラーにedit, updateアクション追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,4 +4,12 @@ class ProfilesController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+  end
 end
