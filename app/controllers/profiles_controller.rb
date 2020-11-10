14de33d03dabd439043a3e7c52a41b093146a3c0
Haruki Hirai <iamprivateengineer@gmail.com>
2020-11-10 15:20:38 +0900

    コントローラーの記述

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,15 +1,38 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!, except: [:show]
 
+  def new
+    @profile = Profile.new    
+  end
+
+  def create
+    @profile = Profile.new(profile_params)
+    if @profile.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def show
-    @user = User.find(params[:id])
+    @profile = Profile.find_by(user_id: params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
+    @profile = Profile.find_by(user_id: params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
+    @profile = Profile.find_by(user_id: params[:id])
+    if @profile.update(profile_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:trans_exp_id, :papa_exp_id, :text).merge(user_id: current_user.id)
   end
 end
