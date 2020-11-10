699f81b233a17c7ed3c5658de8ae9d67ca5e665c
Haruki Hirai <iamprivateengineer@gmail.com>
2020-11-10 15:42:23 +0900

    ドロップダウンに項目追加

--- app/models/trans_exp.rb
+++ app/models/trans_exp.rb
@@ -1,9 +1,10 @@
 class TransExp < ActiveHash::Base
   self.data = [
     { id: 0,  name: '---' },
-    { id: 1,  name: '1〜3年未満' },
-    { id: 2,  name: '3〜5年' },
-    { id: 3,  name: '5〜10年' },
-    { id: 4,  name: '10年以上' },
+    { id: 1,  name: '性転換前' },
+    { id: 2,  name: '0〜3年未満' },
+    { id: 3,  name: '3〜5年' },
+    { id: 4,  name: '5〜10年' },
+    { id: 5,  name: '10年以上' },
   ]
 end
