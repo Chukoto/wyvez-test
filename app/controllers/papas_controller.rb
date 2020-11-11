995bc8d11e19444f29740659f0edaceee521a80c
Haruki Hirai <iamprivateengineer@gmail.com>
2020-11-11 16:50:55 +0900

    papaslコントローラにnew, createアクション追加,記述

--- app/controllers/papas_controller.rb
+++ app/controllers/papas_controller.rb
@@ -1,4 +1,25 @@
 class PapasController < ApplicationController
+before_action :authenticate_user!, except: [:index, :show] 
+
   def index    
+  end
+
+  def new
+    @papa = Papa.new
+  end
+
+  def create
+    @papa = Papa.new(papa_params)
+    if @papa.save
+      redirect_to papas_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def papa_params
+    params.require(:papa).permit(:image, :name, :age_id, :marriage_id, :child_id, :memo).merge(user_id: current_user.id)
   end
 end
