d1b504281ef630565ed3f56a4c4e60c3c8077077
Haruki Hirai <iamprivateengineer@gmail.com>
2020-11-11 19:31:59 +0900

    papasコントローラーにedit, updateアクション記述

--- app/controllers/papas_controller.rb
+++ app/controllers/papas_controller.rb
@@ -1,8 +1,10 @@
 class PapasController < ApplicationController
 before_action :authenticate_user!, except: [:index, :show] 
+before_action :specified_papa, only: [:edit, :update]
+before_action :specified_user, only: [:edit]
 
   def show
-    @papas = Papa.all   
+    @user = User.find(params[:id])
   end
 
   def new
@@ -18,9 +20,28 @@ before_action :authenticate_user!, except: [:index, :show]
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @papa.update(papa_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+
   private
 
   def papa_params
     params.require(:papa).permit(:image, :name, :age_id, :marriage_id, :child_id, :memo).merge(user_id: current_user.id)
+  end
+
+  def specified_papa
+    @papa = Papa.find(params[:id])
+  end
+
+  def specified_user
+    redirect_to root_path unless current_user.id == @papa.user_id
   end
 end
