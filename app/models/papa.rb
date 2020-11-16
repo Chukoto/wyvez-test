dab87ae48724f769231c6fc5ad19ce1d96b8dd79
Haruki Hirai <iamprivateengineer@gmail.com>
2020-11-16 11:16:27 +0900

    モデルにdependant::destroy追記

--- app/models/papa.rb
+++ app/models/papa.rb
@@ -5,7 +5,7 @@ class Papa < ApplicationRecord
   belongs_to_active_hash :child
 
   belongs_to :user
-  has_many :papa_events
+  has_many :papa_events, dependent: :destroy
 
   has_one_attached :image
 
