72deccf19655ceb1ee823942e2c73aa1c5c15f13
Haruki Hirai <iamprivateengineer@gmail.com>
2020-11-16 18:34:33 +0900

    beauty_eventsコントローラーにnew, createアクション追加

--- app/controllers/beauty_events_controller.rb
+++ app/controllers/beauty_events_controller.rb
@@ -1,4 +1,26 @@
 class BeautyEventsController < ApplicationController
+  before_action :authenticate_user!
+
   def show
+    @beauty_events = current_user.beauty_events.where(user_id: current_user.id).order('created_at DESC')
+  end
+
+  def new
+    @beauty_event = BeautyEvent.new
+  end
+
+  def create
+    @beauty_event = BeautyEvent.new(beauty_event_params)
+    if @beauty_event.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def beauty_event_params
+    params.require(:beauty_event).permit(:beauty_cate_id, :started_at, :place, :memo, :cost, :status_id).merge(user_id: current_user.id)    
   end
 end
